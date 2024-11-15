d3cb853ddd99b4fd3937cad9ec7ef980571851c3
h-kobayashi <h-kobayashi@techoes.co.jp>
2024-11-15 18:02:43 +0900

    ✨ コンポーネント間のStateのリフトアップを使った機能を追加

--- components/ExternalLinks/index.jsx
+++ components/ExternalLinks/index.jsx
@@ -1,30 +1,7 @@
-const ITEMS = [
-  {
-    href: "https://nextjs.org/docs",
-    title: "Documentation \u2192",
-    description: "Find in-depth information about Next.js features and API.",
-  },
-  {
-    href: "https://nextjs.org/learn",
-    title: "Learn \u2192",
-    description: "Learn about Next.js in an interactive course with quizzes!",
-  },
-  {
-    href: "https://vercel.com/templates/next.js?utm_source=next-site&utm_medium=navbar&utm_campaign=next_site_nav_templates",
-    title: "Examples \u2192",
-    description: "Discover and deploy boilerplate example Next.js projects.",
-  },
-  {
-    href: "https://vercel.com/home",
-    title: "Deploy \u2192",
-    description: "Instantly deploy your Next.js site to a public URL with vercel",
-  },
-]
-
-export function ExternalLinks() {
+export function ExternalLinks({ items }) {
   return (
     <div className="p-4 rounded grid grid-cols-2 gap-4 w-6/12">
-      {ITEMS.map(item => {
+      {items.map(item => {
         return (
           <a key={item.href} href={item.href} className="p-4 border rounded-lg hover:text-blue-500 hover:border-blue-500" target="_blank">
             <h3 className="text-2xl font-bold p-2 px-4">{item.title}</h3>
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,30 @@ import { Header } from "@/components/Header";
 import { Main } from "@/components/Main";
 import { Footer } from "@/components/Footer";
 import { ExternalLinks } from "@/components/ExternalLinks";
+import { useCallback, useState } from "react";
+
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs",
+    title: "Documentation \u2192",
+    description: "Find in-depth information about Next.js features and API.",
+  },
+  {
+    href: "https://nextjs.org/learn",
+    title: "Learn \u2192",
+    description: "Learn about Next.js in an interactive course with quizzes!",
+  },
+  {
+    href: "https://vercel.com/templates/next.js?utm_source=next-site&utm_medium=navbar&utm_campaign=next_site_nav_templates",
+    title: "Examples \u2192",
+    description: "Discover and deploy boilerplate example Next.js projects.",
+  },
+  {
+    href: "https://vercel.com/home",
+    title: "Deploy \u2192",
+    description: "Instantly deploy your Next.js site to a public URL with vercel",
+  },
+]
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,13 +41,23 @@ const geistMono = localFont({
 
 export default function Home(props) {
   const {count, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd} = props;
+  const [items, setItems] = useState(ITEMS)
+
+  const handleReduce = useCallback(() => {    
+    setItems(prevItems => {
+      return prevItems.slice(0, prevItems.length - 1)
+    })
+  }, [])
 
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <Header />
-      <Main page="index" />
+      <Main page="index"/>
+      <p>items.length = <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">{items.length}</code></p>
+      <button onClick={handleReduce}>itemsを減らす</button>
+      <ExternalLinks items={items}/>
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
@@ -35,7 +69,6 @@ export default function Home(props) {
         })}
       </ul>
       <Footer />
-      <ExternalLinks />
     </div>
   );
 }
