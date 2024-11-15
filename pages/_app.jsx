922712f5e33395533a1e70878bcfeaedd3cae992
h-kobayashi <h-kobayashi@techoes.co.jp>
2024-11-15 18:44:07 +0900

    ⚗️ useRouterを使い背景色の変更を試す

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,11 @@
-import { useBgLightBlue } from "@/hooks/useBgLightBlue";
+import { useBgColor } from "@/hooks/useBgColor";
 import { useCounter } from "@/hooks/useCounter";
 import { useInputArray } from "@/hooks/useInputArray";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  const counter = useCounter()
-  const inputArray = useInputArray()
-  useBgLightBlue()
+  const counter = useCounter();
+  const inputArray = useInputArray();
+  useBgColor();
   return <Component {...pageProps} {...counter} {...inputArray} />;
 }
