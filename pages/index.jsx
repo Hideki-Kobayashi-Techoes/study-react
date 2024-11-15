de8259c43449a14ea4d6bd8b8ad12d5c34c43433
h-kobayashi <h-kobayashi@techoes.co.jp>
2024-11-15 11:45:21 +0900

    ♻️ 状態の変更の際に前の変数にprevをつけた

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,7 +23,7 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount(count => count + 1)
+      setCount(prevCount => prevCount + 1)
     }
   }, [count])
 
@@ -36,7 +36,7 @@ export default function Home() {
   }, [])
 
   const handleDisplay = useCallback(() => {
-    setIsShow(isShow => !isShow)
+    setIsShow(prevIsShow => !prevIsShow)
   }, [])
 
   useEffect(() => {
