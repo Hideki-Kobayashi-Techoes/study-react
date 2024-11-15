a8841c938b9859b5d5df9ba2dd1be94f1497d128
h-kobayashi <h-kobayashi@techoes.co.jp>
2024-11-15 19:14:13 +0900

    ♻️ リファクタリング

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,15 +1,21 @@
 import Link from "next/link";
 import styles from "@/components/Header/Header.module.css"
 
+const NAV_ITEMS = [
+  {href: "/", label: "Index"},
+  {href: "/about", label: "About"},
+]
+
 export function Header() {
   return (
       <header className={styles.header}>
-        <Link href="/" className={`${styles.anchor} hover:underline hover:underline-offset-4`}>
-            Index
-        </Link>
-        <Link href="/about" className={`${styles.anchor} hover:underline hover:underline-offset-4`}>
-            About
-        </Link>
+        {NAV_ITEMS.map((item) => {
+          return (
+            <Link key={item.href} href={item.href} className={`${styles.anchor} hover:underline hover:underline-offset-4`}>
+              {item.label}
+            </Link>
+          )
+        })}
       </header>
   );
 }
