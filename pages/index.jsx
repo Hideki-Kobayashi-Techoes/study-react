84e2944d65b9cc74ac0e36a7523e4852cfca87c7
h-kobayashi <h-kobayashi@techoes.co.jp>
2024-11-15 11:47:09 +0900

    ✨ useStateで配列を追加する処理を実装

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,6 +20,7 @@ export default function Home() {
   const [count, setCount] = useState(1)
   const [text, setText] = useState("")
   const [isShow, setIsShow] = useState(true)
+  const [array, setArray] = useState([])
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -39,6 +40,16 @@ export default function Home() {
     setIsShow(prevIsShow => !prevIsShow)
   }, [])
 
+  const handleAdd = useCallback(() => {
+    setArray(prevArray => {
+      if (prevArray.some(item => item === text)) {
+        alert("同じ要素がすでに存在します")
+        return prevArray
+      }
+      return [...prevArray, text]
+    })
+  }, [text])
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightBlue"
     return () => {
@@ -54,8 +65,14 @@ export default function Home() {
       <Main page="index" />
       <input type="text" value={text} onChange={handleChange}/>
       {isShow ? <h1>{count}</h1> : null}
-      <button href="/about" onClick={handleClick}>ボタン</button>
-      <button href="/about" onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
       <Footer />
       <ExternalLinks />
     </div>
